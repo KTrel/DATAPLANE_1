e269edbbe34745e2b8110775d27799508f26743a
Thomas Holterbach <thomasholterbach@gmail.com>
2016-02-06 23:01:51 +0100

    Added run bash script

---
+++ python/BGPSTream/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+start_ts=1000
+interval=3600
+nbproc=24
+
+for i in `seq 0 $nbproc`;
+do
+    start_tmp=$(($start_ts+($i*$interval)))
+    end_tmp=$(($start_tmp+$interval))
+    python MyBGPStream.py -s $start_tmp -e $end_tmp
+done
+

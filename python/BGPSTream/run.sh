8884eb0c423e5ae1cccbfccbda7b8c35a28d0747
Thomas Holterbach <thomasholterbach@gmail.com>
2016-02-06 23:36:33 +0100

    Improved run script.

--- python/BGPSTream/run.sh
+++ python/BGPSTream/run.sh
@@ -8,6 +8,7 @@ for i in `seq 0 $nbproc`;
 do
     start_tmp=$(($start_ts+($i*$interval)))
     end_tmp=$(($start_tmp+$interval))
+    echo python MyBGPStream.py -s $start_tmp -e $end_tmp &
     python MyBGPStream.py -s $start_tmp -e $end_tmp &
 done
 

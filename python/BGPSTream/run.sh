128449874166166f5d8a86a4073451b782f0daf9
Thomas Holterbach <thomasholterbach@gmail.com>
2016-02-06 23:17:59 +0100

    run background

--- python/BGPSTream/run.sh
+++ python/BGPSTream/run.sh
@@ -8,6 +8,6 @@ for i in `seq 0 $nbproc`;
 do
     start_tmp=$(($start_ts+($i*$interval)))
     end_tmp=$(($start_tmp+$interval))
-    python MyBGPStream.py -s $start_tmp -e $end_tmp
+    python MyBGPStream.py -s $start_tmp -e $end_tmp &
 done
 
